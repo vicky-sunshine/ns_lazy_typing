daa59ab6771ca51aac77c61ce91220ca72c70079
vicky <lch82327@gmail.com>
2015-10-22 04:25:38 +0800

    ns hw finally

--- ns_typing.rb
+++ ns_typing.rb
@@ -1,44 +1,22 @@
-require 'socket'      # Sockets are in standard library
+require 'socket'
+require './ipconfig.rb'
 
-hostname = '140.113.194.85'
-port = 49_169
+# need to fill in basic info in ipconfig.rb
+hostname = HOSTNAME
+port = PORT
 
 s = TCPSocket.open(hostname, port)
-while line = s.gets   # Read lines from the socket
-  puts line.chop      # And print with platform line terminator
-  if line.chop.eql? "3.Exit"
-    puts "\n-----socket msg: entrance show done-----\n\n"
-    break
-  end
-end
-
-s.send("1\r\n", 0)    # send choice to start game
-
-while line = s.gets   # Read lines from the socket
-  puts line.chop      # And print with platform line terminator
-  if line.chop.eql? 'Choose the speed level(1-9):'
-    puts "\n-----socket msg: speed option show done-----\n\n"
-    break
-  end
-end
-
-s.send("9\r\n", 0)    # send choice to start game
-
-puts "\n-----socket msg: start game-----\n\n"
-
-while line = s.gets   # Read lines from the socket
-  if line[0].eql? "S"
-    puts line.chop
-    next
-  end
-
-  lines = line.chop.split('|')
-  unless lines.size.eql? 4
-    next
-  end
+while line = s.gets
+  # Read lines from the socket
   puts line.chop
-  puts lines[2]
-  s.send(lines[2] + "\r\n", 0)
-end
 
-s.close               # Close the socket when done
+  # send choice to start game
+  (line.chop.eql? '3.Exit') && s.send("1\r\n", 0)
+  # send speed setting
+  (line.chop.eql? 'Choose the speed level(1-9):') && s.send("9\r\n", 0)
+
+  # get the word!
+  lines = line.chop.split('|')
+  (lines.size.eql? 4) && s.send(lines[2] + "\r\n", 0)
+end
+s.close
